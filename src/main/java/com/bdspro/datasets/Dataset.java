0dd28e9a796e08617aed3aebb8cd87521689a22e
DennisN23 <dennis.natusch@t-online.de>
2023-06-30 10:31:54 +0200

    added getCsvFileSize

--- src/main/java/com/bdspro/datasets/Dataset.java
+++ src/main/java/com/bdspro/datasets/Dataset.java
@@ -1,5 +1,8 @@
 package com.bdspro.datasets;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +11,14 @@ public interface Dataset {
     String getTableName();
     List<Map.Entry<String, ColumnType>> getColumnNamesWithTypes();
     String getCsvName();
+    default long getCsvFileSize() {
+        try {
+            return Files.size(Paths.get(getCsvName()));
+        }
+        catch (IOException e) {
+            return -1L;
+        }
+    }
     String getTimeStampColumnName();
     String getEntityColumnName();
     String getValueColumnName();
