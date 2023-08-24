cbcb74375e2d3d3e2291bd4f55c768558394db8d
kkulesz <kulesza.konrad@protonmail.com>
2023-08-24 19:01:35 +0200

    fix x-axis in avg-que-latency-selectivity plots

--- visualizer.py
+++ visualizer.py
@@ -86,8 +86,8 @@ def visualize_read_only(json, row_count, batch_size):
     json = filter_json(json, "batchSize", batch_size)
     dbs = getAllDBs(json)
     results_by_db = {}
-    # buckets = [0, 1, 0.0001 * row_count, 0.001 * row_count, 0.01 * row_count, 0.1 * row_count, row_count]
-    buckets = [1, 0.01 * row_count, 0.05 * row_count, 0.1 * row_count, 0.15 * row_count, 0.2 * row_count, row_count]
+    buckets = [0, 1, 0.0001 * row_count, 0.001 * row_count, 0.01 * row_count, 0.1 * row_count, row_count]
+    # buckets = [1, 0.01 * row_count, 0.05 * row_count, 0.1 * row_count, 0.15 * row_count, 0.2 * row_count, row_count]
     for db in dbs:
         latencies_by_selectivity = {}
         for bucket in buckets:
@@ -108,8 +108,8 @@ def visualize_read_only(json, row_count, batch_size):
         results_by_db[db] = latencies
     x = numpy.array(range(len(buckets)))  # numpy.array(buckets)
     ax = plt.subplot(111)
-    # plt.xticks(ticks=range(len(x)), labels=["0", "1", "<0.01%", "<0.1%", "<1%", "<10%", ">=10%"], rotation=45)
-    plt.xticks(ticks=range(len(x)), labels=["1", "<1%", "<5%", "<10%", "<15%", "<20%", ">=25%"], rotation=45)
+    plt.xticks(ticks=range(len(x)), labels=["0", "1", "<0.01%", "<0.1%", "<1%", "<10%", ">=10%"], rotation=45)
+    # plt.xticks(ticks=range(len(x)), labels=["1", "<1%", "<5%", "<10%", "<15%", "<20%", ">=25%"], rotation=45)
     ax.bar(x - 0.1, results_by_db[dbs[0]], width=.2, color='b', align='center', label=dbs[0])
     ax.bar(x + 0.1, results_by_db[dbs[1]], width=.2, color='r', align='center', label=dbs[1])
     plt.legend(loc="upper left")
